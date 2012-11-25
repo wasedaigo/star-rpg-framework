33f790302b2b12686f70793d63f6dcc6e474a149
Daigo Sato <sato.daigo@52040-MAC-6.local>
2012-11-25 20:02:35 +0700

    Fix wrong comment

--- apps/sample/src/Main.ts
+++ apps/sample/src/Main.ts
@@ -2,10 +2,7 @@
 ///<reference path='Scene_Title.ts'/>
 
 /**
- * DataManager
- *
- * データベースとゲームオブジェクトを管理するモジュールです。ゲームで使用する
- * ほぼ全てのグローバル変数はこのモジュールで初期化されます。
+ * Starting point of the application
  */
 function main(config) {
 	rpg2d.SceneDirector.runWithScene(new Scene_Title(), config);
