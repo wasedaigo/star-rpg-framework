c0e1f89e709dd1ba6ae44616e9faf33df9591244
Hajime Hoshi <hajimehoshi@gmail.com>
2012-11-30 02:23:29 +0900

    Modified indentation

--- apps/sample/src/Scene_Title.ts
+++ apps/sample/src/Scene_Title.ts
@@ -7,10 +7,10 @@
  *
  */
 class Scene_Title extends Scene_Base {
-	public start() {
-		var label = cc.LabelTTF.create("Title Scene", "Arial", 20);
-		label.setPosition(cc.p(160, 450));
-		this.addChild(label);
+    public start() {
+	var label = cc.LabelTTF.create("Title Scene", "Arial", 20);
+	label.setPosition(cc.p(160, 450));
+	this.addChild(label);
 
         var label: cc.LabelTTF = cc.LabelTTF.create("Go to Map", "Arial", 20);
         var menuItem: cc.MenuItemLabel = cc.MenuItemLabel.create(label, this.onClicked, this);
@@ -19,12 +19,12 @@ class Scene_Title extends Scene_Base {
         menu.setPosition(cc.PointZero());
         menuItem.setPosition(cc.p(s.width / 2, s.height / 2));
         this.addChild(menu);
-	}
+    }
 
-	private onClicked() {
-		cc.Director.getInstance().replaceScene(new Scene_Map());
-	}
-	
-	private terminate() {
-	}
+    private onClicked() {
+	cc.Director.getInstance().replaceScene(new Scene_Map());
+    }
+    
+    private terminate() {
+    }
 }
--- apps/sample/src/Spriteset_Map.ts
+++ apps/sample/src/Spriteset_Map.ts
@@ -7,44 +7,44 @@
  *
  */
 class Spriteset_Map {
-  private _root: cc.Scene = null;
-  private _map: cc.TMXTiledMap = null;
-  private _character: Sprite_Character = null;
+    private _root: cc.Scene = null;
+    private _map: cc.TMXTiledMap = null;
+    private _character: Sprite_Character = null;
 
-  /**
-  * Initialize all layers
-  */
-  constructor(root:cc.Scene) {
-    this._root = root;
-    this.createTilemap();
-    this.createCharacters();
-    /*
-    create_parallax
-    create_characters
-    create_shadow
-    create_weather
-    create_pictures
-    create_timer
-    update
-    */
-  }
+    /**
+     * Initialize all layers
+     */
+    constructor(root:cc.Scene) {
+        this._root = root;
+        this.createTilemap();
+        this.createCharacters();
+        /*
+          create_parallax
+          create_characters
+          create_shadow
+          create_weather
+          create_pictures
+          create_timer
+          update
+        */
+    }
 
-  /**
-  * Dispose all layers
-  */
-  public dispose() {
-    this._root.removeChild(this._map);
-    this._character.dispose();
-  }
+    /**
+     * Dispose all layers
+     */
+    public dispose() {
+        this._root.removeChild(this._map);
+        this._character.dispose();
+    }
 
-  private createTilemap() {
-    //Display TileMap
-    this._map = cc.TMXTiledMap.create("res/tmx/sample.tmx");
-    this._map.setPosition(cc.p(0, 0));
-    this._root.addChild(this._map, -1, 1);    
-  }
+    private createTilemap() {
+        //Display TileMap
+        this._map = cc.TMXTiledMap.create("res/tmx/sample.tmx");
+        this._map.setPosition(cc.p(0, 0));
+        this._root.addChild(this._map, -1, 1);    
+    }
 
-  private createCharacters() {
-    this._character = new Sprite_Character(this._root);
-  }
+    private createCharacters() {
+        this._character = new Sprite_Character(this._root);
+    }
 }
