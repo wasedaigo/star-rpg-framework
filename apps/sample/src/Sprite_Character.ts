88543cda824849928675be7be382b275be66e1b8
Daigo Sato <sato.daigo@52040-MAC-6.local>
2012-12-02 22:39:28 +0900

    Refactoring

--- apps/sample/src/Sprite_Character.ts
+++ apps/sample/src/Sprite_Character.ts
@@ -7,15 +7,22 @@
  *
  */
 class Sprite_Character {
-    private static TAG_SPRITE_MANAGER:number = 1;
-    private static CYCLE_INTERVAL:number = 0.2;
-    private _batch:cc.SpriteBatchNode;
-    private _root:cc.Scene;
-    private _sprite:cc.Sprite;
-    private _anim:number;
-    private _dir:number;
-    private _cycleDir:number;
-    private _cycleTimer:number;
+    private static WALK_ANIM_PATTERN_COUNT: number = 3;
+    private static TAG_SPRITE_MANAGER: number = 1;
+    private static CycleInterval = {
+        VerySlow: 0.5,
+        Slow    : 0.4,
+        Normal  : 0.3,
+        Fast    : 0.2,
+        VeryFast: 0.1
+    };
+    private _root: cc.Scene;
+    private _sprite: cc.Sprite;
+    private _anim: number;
+    private _dir: number;
+    private _cycleDir: number;
+    private _cycleTimer: number;
+    private _movingTarget: ebi.Point;
     /**
     * Initialize all layers
     */
@@ -44,28 +51,29 @@ class Sprite_Character {
     * Dispose all layers
     */
     public dispose() {
-        this._root.removeChild(this._batch);
+        this._root.removeChild(this._sprite);
     }
 
-    public update(dt:number) {
-        this._updateFrame(dt);
+    public update(dt: number) {
+        if (this._updateWalkCycleStep(dt)) {
+            this._updateFrame();
+        }
     }
 
-    // Update character frame
-    private _updateFrame(dt:number) {
+    // Update walk-cycle of the character
+    private _updateWalkCycleStep(dt: number) {
         this._cycleTimer += dt;
-        if (this._cycleTimer < Sprite_Character.CYCLE_INTERVAL) {
-            return;
+        if (this._cycleTimer < Sprite_Character.CycleInterval.Normal) {
+            return false;
         }
         this._cycleTimer = 0;
-
-        this._nextFrame();
+        return true;   
     }
 
     // Set next frame of the character
-    private _nextFrame() {
+    private _updateFrame() {
         this._anim += this._cycleDir;
-        if (this._anim === 2 || this._anim === 0) {
+        if (this._anim === (Sprite_Character.WALK_ANIM_PATTERN_COUNT - 1) || this._anim === 0) {
             this._cycleDir *= -1;
         }
 
