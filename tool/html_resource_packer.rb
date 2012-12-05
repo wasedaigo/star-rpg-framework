15395b69de896ab16e237d4838e0d4ff0f313333
Hajime Hoshi <hajimehoshi@gmail.com>
2012-12-06 02:57:22 +0900

    Modified indentations

--- tool/html_resource_packer.rb
+++ tool/html_resource_packer.rb
@@ -9,19 +9,19 @@ list = [
 	{"ext" => "png", "type" => "image"},
 	{"ext" => "tmx", "type" => "tmx"},
 	{"ext" => "wav", "type" => "effect"}
-]
+       ]
 
 list.each do |desc|
-	pathes = Dir["#{input_dir}/**/*.#{desc['ext']}"]
-	pathes.each do |path|
-		str = path.gsub(input_dir + "/", "res/")
-		result << {"type" => desc['type'] ,"src" => str}
-	end
+  pathes = Dir["#{input_dir}/**/*.#{desc['ext']}"]
+  pathes.each do |path|
+    str = path.gsub(input_dir + "/", "res/")
+    result << {"type" => desc['type'] ,"src" => str}
+  end
 end
 
 content = "var g_ressources = "+ result.to_json + ";"
 path = "#{output_dir}/resources.js"
 File.open(path, 'w') do |f| 
-	f.write(content)
-	p "Resource Packed at #{path}"
+  f.write(content)
+  p "Resource Packed at #{path}"
 end
