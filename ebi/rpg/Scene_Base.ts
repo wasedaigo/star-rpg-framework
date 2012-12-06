aeab7ec331192cd8ec02125eced069f4b249f888
Hajime Hoshi <hajimehoshi@gmail.com>
2012-12-07 03:00:27 +0900

    Modified Scene_Base.ts

--- ebi/rpg/Scene_Base.ts
+++ ebi/rpg/Scene_Base.ts
@@ -1,6 +1,28 @@
 /// <reference path='../../cc/cocos2d.d.ts' />
 /// <reference path='../game/Input.ts' />
 
+// 仮
+module ebi {
+    export module game {
+        export class Graphics {
+            public static update(): void {}
+            public static freeze(): void {}
+            public static transition(duration:number): void {}
+        }
+    }
+}
+
+module ebi {
+    export module rpg {
+        export class SceneManager {
+            public static get scene(): Scene_Base {
+                return null;
+            }
+            public static returnScene(): void {}
+        }
+    }
+}
+
 module ebi {
 
     export module rpg {
@@ -14,12 +36,73 @@ module ebi {
         export class Scene_Base extends cc.Scene {
             // TODO: Do Not inherit cc.Scene
 
-            public start(): void {}
-            public terminate(): void {}
-
-            public update(dt:number): void {
-	        ebi.game.Input.update();
+            public main(): void {
+                // この実装、 Fiber 前提になっていると思われるので変えましょう
+                this.start();
+                this.postStart();
+                if (this.isSceneChanging) {
+                    this.update(0); // 適当な数字を入れてる
+                }
+                this.preTerminate();
+                this.terminate();
             }
+
+            public start(): void {
+            }
+
+            public postStart(): void {
+                this.performTransition();
+                ebi.game.Input.update();
+            }
+
+            public get isSceneChanging(): bool {
+                return SceneManager.scene != this;
+            }
+
+            public update(t:number): void {
+                this.updateBasic();
+            }
+           
+            public updateBasic(): void {
+                ebi.game.Graphics.update();
+                ebi.game.Input.update();
+                this.updateAllWindows();
+            }
+
+            public preTerminate(): void {
+            }
+
+            public terminate(): void {
+                ebi.game.Graphics.freeze();
+                this.disposeAllWindows();
+            }
+
+            public performTransition(): void {
+                ebi.game.Graphics.transition(this.transitionSpeed);
+            }
+
+            public get transitionSpeed(): number {
+                return 10;
+            }
+
+            public updateAllWindows(): void {
+                // TODO: Object.keys とかで列挙して実装?
+            }
+
+            public disposeAllWindows(): void {
+                // TODO: Object.keys とかで列挙して実装?
+            }
+
+            public returnScene(): void {
+                SceneManager.returnScene();
+            }
+
+            public fadeoutAll(time = 1000): void {
+            }
+
+            // TODO: checkGameover の実装?
+
+            // 以下 Cocos 用
 
             public onEnter(): void {
                 super.onEnter();
