c8674a553f29d7adf6a03021262bae7d3fd37c41
Hajime Hoshi <hajimehoshi@gmail.com>
2012-12-07 02:08:16 +0900

    Removed ebi.game.Scene

--- ebi/rpg/Scene_Base.ts
+++ ebi/rpg/Scene_Base.ts
@@ -1,5 +1,5 @@
+/// <reference path='../../cc/cocos2d.d.ts' />
 /// <reference path='../game/Input.ts' />
-/// <reference path='../game/Scene.ts' />
 
 module ebi {
 
@@ -11,11 +11,32 @@ module ebi {
          * シーンのベースとなります
          *
          */
-        export class Scene_Base extends ebi.game.Scene {
+        export class Scene_Base extends cc.Scene {
+            // TODO: Do Not inherit cc.Scene
+
+            public start(): void {}
+            public terminate(): void {}
 
             public update(dt:number): void {
 	        ebi.game.Input.update();
             }
+
+            public onEnter(): void {
+                super.onEnter();
+                this.scheduleUpdate();
+                this.start();
+
+                var layer = new ebi.game.InputLayer();
+                layer.init();
+                layer.setTouchEnabled(true);
+                this.addChild(layer);
+            }
+
+            public onExit(): void {
+                super.onExit();
+                this.terminate();
+            }
+
         }
 
     }
