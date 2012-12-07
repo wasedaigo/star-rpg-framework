e12a000e201c7503f1287238b51c978300ee2395
Daigo Sato <sato.daigo@52040-MAC-6.local>
2012-12-07 22:11:35 +0900

    Place ruby code instead of ts

--- ebi/rpg_port/Sprite_Base.rb
+++ ebi/rpg_port/Sprite_Base.rb
@@ -1,50 +1,50 @@
-//==============================================================================
-// ■ Sprite_Base
-//------------------------------------------------------------------------------
-// 　アニメーションの表示処理を追加したスプライトのクラスです。
-//==============================================================================
+#==============================================================================
+# ■ Sprite_Base
+#------------------------------------------------------------------------------
+# 　アニメーションの表示処理を追加したスプライトのクラスです。
+#==============================================================================
 
 class Sprite_Base < Sprite
-  //--------------------------------------------------------------------------
-  // ● クラス変数
-  //--------------------------------------------------------------------------
+  #--------------------------------------------------------------------------
+  # ● クラス変数
+  #--------------------------------------------------------------------------
   @@ani_checker = []
   @@ani_spr_checker = []
   @@_reference_count = {}
-  //--------------------------------------------------------------------------
-  // ● オブジェクト初期化
-  //--------------------------------------------------------------------------
-  public initialize(viewport = nil)
+  #--------------------------------------------------------------------------
+  # ● オブジェクト初期化
+  #--------------------------------------------------------------------------
+  def initialize(viewport = nil)
     super(viewport)
-    @use_sprite = true        // スプライト使用フラグ
-    @ani_duration = 0         // アニメーションの残り時間
-  }
-  //--------------------------------------------------------------------------
-  // ● 解放
-  //--------------------------------------------------------------------------
-  public dispose
+    @use_sprite = true        # スプライト使用フラグ
+    @ani_duration = 0         # アニメーションの残り時間
+  end
+  #--------------------------------------------------------------------------
+  # ● 解放
+  #--------------------------------------------------------------------------
+  def dispose
     super
     dispose_animation
-  }
-  //--------------------------------------------------------------------------
-  // ● フレーム更新
-  //--------------------------------------------------------------------------
-  public update
+  end
+  #--------------------------------------------------------------------------
+  # ● フレーム更新
+  #--------------------------------------------------------------------------
+  def update
     super
     update_animation
     @@ani_checker.clear
     @@ani_spr_checker.clear
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーション表示中判定
-  //--------------------------------------------------------------------------
-  public animation?
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーション表示中判定
+  #--------------------------------------------------------------------------
+  def animation?
     @animation != nil
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーションの開始
-  //--------------------------------------------------------------------------
-  public start_animation(animation, mirror = false)
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーションの開始
+  #--------------------------------------------------------------------------
+  def start_animation(animation, mirror = false)
     dispose_animation
     @animation = animation
     if @animation
@@ -54,18 +54,18 @@ class Sprite_Base < Sprite
       load_animation_bitmap
       make_animation_sprites
       set_animation_origin
-    }
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーションの速度を設定
-  //--------------------------------------------------------------------------
-  public set_animation_rate
-    @ani_rate = 4     // デフォルトでは固定値
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーション グラフィックの読み込み
-  //--------------------------------------------------------------------------
-  public load_animation_bitmap
+    end
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーションの速度を設定
+  #--------------------------------------------------------------------------
+  def set_animation_rate
+    @ani_rate = 4     # デフォルトでは固定値
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーション グラフィックの読み込み
+  #--------------------------------------------------------------------------
+  def load_animation_bitmap
     animation1_name = @animation.animation1_name
     animation1_hue = @animation.animation1_hue
     animation2_name = @animation.animation2_name
@@ -76,38 +76,38 @@ class Sprite_Base < Sprite
       @@_reference_count[@ani_bitmap1] += 1
     else
       @@_reference_count[@ani_bitmap1] = 1
-    }
+    end
     if @@_reference_count.include?(@ani_bitmap2)
       @@_reference_count[@ani_bitmap2] += 1
     else
       @@_reference_count[@ani_bitmap2] = 1
-    }
+    end
     Graphics.frame_reset
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーションスプライトの作成
-  //--------------------------------------------------------------------------
-  public make_animation_sprites
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーションスプライトの作成
+  #--------------------------------------------------------------------------
+  def make_animation_sprites
     @ani_sprites = []
     if @use_sprite && !@@ani_spr_checker.include?(@animation)
       16.times do
         sprite = ::Sprite.new(viewport)
         sprite.visible = false
         @ani_sprites.push(sprite)
-      }
+      end
       if @animation.position == 3
         @@ani_spr_checker.push(@animation)
-      }
-    }
+      end
+    end
     @ani_duplicated = @@ani_checker.include?(@animation)
     if !@ani_duplicated && @animation.position == 3
       @@ani_checker.push(@animation)
-    }
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーションの原点設定
-  //--------------------------------------------------------------------------
-  public set_animation_origin
+    end
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーションの原点設定
+  #--------------------------------------------------------------------------
+  def set_animation_origin
     if @animation.position == 3
       if viewport == nil
         @ani_ox = Graphics.width / 2
@@ -115,7 +115,7 @@ class Sprite_Base < Sprite
       else
         @ani_ox = viewport.rect.width / 2
         @ani_oy = viewport.rect.height / 2
-      }
+      end
     else
       @ani_ox = x - ox + width / 2
       @ani_oy = y - oy + height / 2
@@ -123,37 +123,37 @@ class Sprite_Base < Sprite
         @ani_oy -= height / 2
       elsif @animation.position == 2
         @ani_oy += height / 2
-      }
-    }
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーションの解放
-  //--------------------------------------------------------------------------
-  public dispose_animation
+      end
+    end
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーションの解放
+  #--------------------------------------------------------------------------
+  def dispose_animation
     if @ani_bitmap1
       @@_reference_count[@ani_bitmap1] -= 1
       if @@_reference_count[@ani_bitmap1] == 0
         @ani_bitmap1.dispose
-      }
-    }
+      end
+    end
     if @ani_bitmap2
       @@_reference_count[@ani_bitmap2] -= 1
       if @@_reference_count[@ani_bitmap2] == 0
         @ani_bitmap2.dispose
-      }
-    }
+      end
+    end
     if @ani_sprites
       @ani_sprites.each {|sprite| sprite.dispose }
       @ani_sprites = nil
       @animation = nil
-    }
+    end
     @ani_bitmap1 = nil
     @ani_bitmap2 = nil
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーションの更新
-  //--------------------------------------------------------------------------
-  public update_animation
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーションの更新
+  #--------------------------------------------------------------------------
+  def update_animation
     return unless animation?
     @ani_duration -= 1
     if @ani_duration % @ani_rate == 0
@@ -163,23 +163,23 @@ class Sprite_Base < Sprite
         animation_set_sprites(@animation.frames[frame_index])
         @animation.timings.each do |timing|
           animation_process_timing(timing) if timing.frame == frame_index
-        }
+        end
       else
         end_animation
-      }
-    }
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーションの終了
-  //--------------------------------------------------------------------------
-  public end_animation
+      end
+    end
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーションの終了
+  #--------------------------------------------------------------------------
+  def end_animation
     dispose_animation
-  }
-  //--------------------------------------------------------------------------
-  // ● アニメーションスプライトの設定
-  //     frame : フレームデータ（RPG::Animation::Frame）
-  //--------------------------------------------------------------------------
-  public animation_set_sprites(frame)
+  end
+  #--------------------------------------------------------------------------
+  # ● アニメーションスプライトの設定
+  #     frame : フレームデータ（RPG::Animation::Frame）
+  #--------------------------------------------------------------------------
+  def animation_set_sprites(frame)
     cell_data = frame.cell_data
     @ani_sprites.each_with_index do |sprite, i|
       next unless sprite
@@ -187,7 +187,7 @@ class Sprite_Base < Sprite
       if !pattern || pattern < 0
         sprite.visible = false
         next
-      }
+      end
       sprite.bitmap = pattern < 100 ? @ani_bitmap1 : @ani_bitmap2
       sprite.visible = true
       sprite.src_rect.set(pattern % 5 * 192,
@@ -202,7 +202,7 @@ class Sprite_Base < Sprite
         sprite.y = @ani_oy + cell_data[i, 2]
         sprite.angle = cell_data[i, 4]
         sprite.mirror = (cell_data[i, 5] == 1)
-      }
+      end
       sprite.z = self.z + 300 + i
       sprite.ox = 96
       sprite.oy = 96
@@ -210,13 +210,13 @@ class Sprite_Base < Sprite
       sprite.zoom_y = cell_data[i, 3] / 100.0
       sprite.opacity = cell_data[i, 6] * self.opacity / 255.0
       sprite.blend_type = cell_data[i, 7]
-    }
-  }
-  //--------------------------------------------------------------------------
-  // ● SE とフラッシュのタイミング処理
-  //     timing : タイミングデータ（RPG::Animation::Timing）
-  //--------------------------------------------------------------------------
-  public animation_process_timing(timing)
+    end
+  end
+  #--------------------------------------------------------------------------
+  # ● SE とフラッシュのタイミング処理
+  #     timing : タイミングデータ（RPG::Animation::Timing）
+  #--------------------------------------------------------------------------
+  def animation_process_timing(timing)
     timing.se.play unless @ani_duplicated
     case timing.flash_scope
     when 1
@@ -224,9 +224,9 @@ class Sprite_Base < Sprite
     when 2
       if viewport && !@ani_duplicated
         viewport.flash(timing.flash_color, timing.flash_duration * @ani_rate)
-      }
+      end
     when 3
       self.flash(nil, timing.flash_duration * @ani_rate)
-    }
-  }
-}
+    end
+  end
+end
