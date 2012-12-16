0c83a021d9739f6f80614940b9648d04f5c2fa44
Daigo Sato <sato.daigo@52040-MAC-6.local>
2012-12-16 13:41:22 +0900

    Improved ruby->ts convert script

--- tool/port_helper.rb
+++ tool/port_helper.rb
@@ -23,19 +23,102 @@ str.gsub!('#', '//')
 str.gsub!('  ', '    ')
 str.gsub!(/def (.*)$/, 'public \1(): any {')
 str.gsub!(/class (.*)$/, 'export class \1 {')
+str.gsub!(/module (.*)/, 'module \1 {')
+
+# Add bracket for if statement
 str.gsub!(/if (.*)$/, 'if (\1) {')
+
+# convert unless keyward into if!
+str.gsub!(/unless (.*)$/, 'if !(\1) {')
+
+# convert else
 str.gsub!('else', '} else {')
-str.gsub!(/(\W)end\b/, '\1}' + "\n")
+
+# convert end
+str.gsub!(/(\W)end/, '\1}')
+
+# nil -> null
 str.gsub!(/\Wnil(\b)/, ' null')
-str.gsub!(/\$(.*)\b/, 'DataManager.\1')
-str.gsub!(/([\w\]\)])$/, '\1;')
+
+# Global variable is Datamanager
+#str.gsub!(/\$(.*)\b/, 'DataManager.\1')
+
+# Swap position of if statement
+str.gsub!(/^(\s*)(.*\w+.*)(if.*\{)$/, '\1\3'+ "\n" + '\1    \2' + "\n" +'\1}')
+
+# Convert new statement
+str.gsub!(/(\w*)\.new\s*(.*)$/, 'new \1(\2')
+
+# Convert do statement
+str.gsub!(/\)\s*do\s*\|(.*)\|/, ', (\1) => {')
+
+# Add ; in the end of the line
+str.gsub!(/([\w\]\)])\s*$/, '\1;')
+
+# Convert forgotten do
+str.gsub!(/\(\s*do\s*;/, '(() => {')
 
 ("a".."z").each do |v|
 	str.gsub!(/_#{v}/, v.upcase)
 end
-str.gsub!(/\@(\w*)\b/, 'this.\1_')
-str.gsub!("public initialize(): any {", "constructor() {")
 
-str.gsub!(/attrReader.*\:(\w*)\b/, 'public get \1(): any {return this.\1_;}');
+# Generate members, reader, accessor
+members = str.map{ |s| s.scan(/\@(\w*)\b/) }.flatten.uniq
+accessors = str.scan(/attrAccessor(.*)$/).flatten
+readers = str.scan(/attrReader(.*)$/).flatten
+accessors = accessors.map {|s| s.scan(/(?:\:(\w*)[\,\;])/)}.flatten.uniq
+readers = readers.map {|s| s.scan(/(?:\:(\w*)[\,\;])/)}.flatten.uniq
+
+str.gsub!(/attrReader.*\:(\w*)\b;/, '');
+str.gsub!(/attrAccessor.*\:(\w*)\b;/, '');
+
+# Categorize private and public
+publics = []
+privates = []
+members.each do |member| 
+	if accessors.include?(member)
+		publics << member
+	else
+		privates << member
+	end
+end
+
+member_str = "
+//
+// private members
+//
+"
+privates.each do |member|
+	member_str += "private" + " #{member}_:any;\n"
+end
+
+member_str += "
+//
+// public members
+//
+"
+publics.each do |member|
+	member_str += "public" + " #{member}:any;\n"
+end
+
+member_str += "
+//
+// accessors
+//
+"
+readers.each do |member|
+	member_str += "public get #{member}(): any {return this.#{member}_;}\n"
+end
+
+str = member_str + str
+
+str.gsub!(/self(\W)/, 'this\1')
+str.gsub!(/\@(\w*)\b/, 'this.\1_')
+str.gsub!(/public initialize\((.*)\): any \{/, 'constructor(\1) {')
+str.gsub!(")()", ')')
+
+("a".."z").each do |v|
+	str.gsub!(/\.#{v}(\w*)\?/, ".is" + v.upcase + '\1')
+end
 
 puts str
