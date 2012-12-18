8d4e014c9002e943c3694edb78b84ee75a41eb64
Hajime Hoshi <hajime_hoshi@qteras.co.jp>
2012-12-18 15:40:33 +0900

    Refactoring

--- script/clean.js
+++ script/clean.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 
 var paths = ['./html/resource.js', './html/game.js', './html/game.js.map'];
 paths.forEach(function (path) {
-    console.log("Removing " + path);
-    fs.unlink(path);
+    console.log("Removing ", path);
+    if (fs.existsSync(path)) {
+        fs.unlinkSync(path);
+    }
 });
