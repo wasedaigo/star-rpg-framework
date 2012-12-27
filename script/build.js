a6ad6b0d8c5d3e9153e256ebedf71df8f2ff1b5f
Hajime Hoshi <hajimehoshi@gmail.com>
2012-12-28 04:41:06 +0900

    Refactoring

--- script/build.js
+++ script/build.js
@@ -2,26 +2,21 @@
 
 var exec = require('child_process').exec;
 
-/*
- * Execute commands synchronously.
- */
-function execCommands(commands) {
-    if (commands.length === 0) {
-        return;
-    }
-    var command = commands[0];
-    console.log(command);
-    exec(command, function (error, stdout, stderr) {
+function createCallback(next) {
+    return function (error, stdout, stderr) {
         if (error) {
             console.error(error.toString());
             return;
         }
-        execCommands(commands.slice(1));
-    });
+        if (next === void(0)) {
+            return;
+        }
+        next();
+    };
 }
 
-var commands = [
-    'node tool/html_resource_packer.js ./html/res > ./html/resources.js',
-    'tsc --sourcemap ./src/ebi/rpg/Main.ts --out ./html/game.js --target ES5',
-];
-execCommands(commands);
+console.log('Generating ./html/resources.js');
+exec('node tool/html_resource_packer.js ./html/res > ./html/resources.js', createCallback(function () {
+    console.log('Compiling ./src/ebi/rpg/Main.ts to ./html/game.js');
+    exec('tsc --sourcemap ./src/ebi/rpg/Main.ts --out ./html/game.js --target ES5', createCallback());
+}));
