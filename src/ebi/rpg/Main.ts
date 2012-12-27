0806f27297b04bf35fed859c42f2756467e36827
Hajime Hoshi <hajimehoshi@gmail.com>
2012-12-28 04:28:47 +0900

    Modified indentations

--- src/ebi/rpg/Main.ts
+++ src/ebi/rpg/Main.ts
@@ -1,31 +1,29 @@
 /// <reference path='../game/Game.ts' />
 
-module ebi {
-    export module rpg {
+module ebi.rpg {
 
-        export class Main {
+    export class Main {
 
-            public static main(): void {
-                ebi.game.Game.run(loop);
+        public static main(): void {
+            ebi.game.Game.run(loop);
+        }
+
+        private static image: ebi.game.Image = null;
+        private static sprite: ebi.game.Sprite = null;
+
+        private static loop(): void {
+            if (!image) {
+                ebi.game.Image.load('res/images/game/title.png', (loadedImage) => {
+                    image = loadedImage;
+                    console.log('loaded!');
+                });
             }
-
-            private static image: ebi.game.Image = null;
-            private static sprite: ebi.game.Sprite = null;
-
-            private static loop(): void {
-                if (!image) {
-                    ebi.game.Image.load('res/images/game/title.png', (loadedImage) => {
-                        image = loadedImage;
-                        console.log('loaded!');
-                    });
-                }
-                if (!sprite) {
-                    sprite = new ebi.game.Sprite(image);
-                }
-                sprite.x++;
+            if (!sprite) {
+                sprite = new ebi.game.Sprite(image);
             }
-
+            sprite.x++;
         }
 
     }
+
 }
