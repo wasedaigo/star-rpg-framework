dcfc91ab445e762edf8c328ebec12db4fa5890f3
Hajime Hoshi <hajimehoshi@gmail.com>
2012-12-30 19:52:55 +0900

    Refactoring

--- src/ebi/rpg/MapCharacter.ts
+++ src/ebi/rpg/MapCharacter.ts
@@ -9,19 +9,19 @@ module ebi.rpg {
         private dir_: number = 0;
         private timer_: number = 0;
         private sprite_: ebi.game.Sprite = null;
-        private charaChipsetData_: any;
+        private charaChipsetData_: Object;
 
         constructor(id: number) {
             this.charaChipsetData_ = DatabaseManager.getCharaChipsetData(id);
-            var image = ResourceManager.getImage(this.charaChipsetData_.srcImage);
+            var image = ResourceManager.getImage(this.charaChipsetData_['srcImage']);
             var data = this.charaChipsetData_;
-            this.frameCount_ = data.startAnim;
-            this.dir_ = data.startDir;
+            this.frameCount_ = data['startAnim'];
+            this.dir_ = data['startDir'];
             this.sprite_ = new ebi.game.Sprite(image);
-            this.sprite_.srcX      = (data.charaX * data.animCount + this.frameCount_) * data.srcWidth;
-            this.sprite_.srcY      = (data.charaY * data.dirCount + this.dir_) * data.srcHeight;
-            this.sprite_.srcWidth  = data.srcWidth;
-            this.sprite_.srcHeight = data.srcHeight;
+            this.sprite_.srcX      = (data['charaX'] * data['animCount'] + this.frameCount_) * data['srcWidth'];
+            this.sprite_.srcY      = (data['charaY'] * data['dirCount'] + this.dir_) * data['srcHeight'];
+            this.sprite_.srcWidth  = data['srcWidth'];
+            this.sprite_.srcHeight = data['srcHeight'];
         }
 
         // x
@@ -49,8 +49,8 @@ module ebi.rpg {
                 this.frameCount_ += this.a_;
 
                 // ----> switching the animation to the right
-                if (this.frameCount_ >= this.charaChipsetData_.animCount - 1) {
-                    this.frameCount_ = this.charaChipsetData_.animCount - 1;
+                if (this.frameCount_ >= this.charaChipsetData_['animCount'] - 1) {
+                    this.frameCount_ = this.charaChipsetData_['animCount'] - 1;
                     this.a_ = -1;
                 }
 
@@ -64,10 +64,10 @@ module ebi.rpg {
             var data = this.charaChipsetData_;
 
             // Update animation frame
-            this.sprite_.srcX = (data.charaX * data.animCount + this.frameCount_) * data.srcWidth;
+            this.sprite_.srcX = (data['charaX'] * data['animCount'] + this.frameCount_) * data['srcWidth'];
 
             // Update animation dir
-            this.sprite_.srcY = (data.charaY * data.dirCount + this.dir_) * data.srcHeight;
+            this.sprite_.srcY = (data['charaY'] * data['dirCount'] + this.dir_) * data['srcHeight'];
         }
     }
 }
