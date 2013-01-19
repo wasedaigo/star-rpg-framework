a424574bd24f5a89dc4fac4abeedef984611f320
Daigo Sato <sato.daigo@52040-MAC-6.local>
2013-01-19 23:30:47 +0900

    RemovedRemoved magic numbers

--- src/ebi/rpg/AnalogInputIndicator.ts
+++ src/ebi/rpg/AnalogInputIndicator.ts
@@ -2,6 +2,7 @@
 module ebi.rpg {
     export class AnalogInputIndicator
      {
+        private static StickRadius: number = 12;
         private static BarImage: string = "system/analog_base";
         private static StickImage: string = "system/analog_stick";
         private base_: ebi.game.Sprite = null;
@@ -13,8 +14,8 @@ module ebi.rpg {
         }
 
         public update(): void {
-            var dx = AnalogInputController.inputDx * 12;
-            var dy = AnalogInputController.inputDy * 12;
+            var dx = AnalogInputController.inputDx * AnalogInputIndicator.StickRadius;
+            var dy = AnalogInputController.inputDy * AnalogInputIndicator.StickRadius;
             this.base_.x = AnalogInputController.touchX - this.base_.srcWidth / 2;
             this.base_.y = AnalogInputController.touchY - this.base_.srcHeight / 2;
             this.stick_.x = AnalogInputController.touchX - this.stick_.srcWidth / 2 + dx;
