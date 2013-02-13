2d423463bb98ab445bb6808186d8299fa974b01d
Daigo Sato <sato.daigo@52040-MAC-6.local>
2013-02-13 21:51:40 +0900

    Forming the base of the tool

--- tool/star_rpg_editor/star_rpg_editor/mainwindow.cpp
+++ tool/star_rpg_editor/star_rpg_editor/mainwindow.cpp
@@ -1,15 +1,30 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pixmap;
+    pixmap.load("/Users/sato.daigo/Development/git/star-rpg-framework/html/res/tmx/images/tile_a.png");
+
+    QPixmap  pixmap2 = QPixmap(256, 1024);
+    QPainter pixPaint(&pixmap2);
+    pixPaint.drawPixmap(QRect(0, 0, 256, 256), pixmap, QRect(0, 0, 256, 256));
+    pixPaint.drawPixmap(QRect(0, 256, 256, 256), pixmap, QRect(256, 0, 256, 256));
 
     QGraphicsScene *scene = new QGraphicsScene();
     ui->graphicsView->setScene(scene);
-    scene->addLine( QLineF( 10, 10, 20, 20 ) );
+    scene->addPixmap(pixmap2);
+
+    QFile qss(":/qss/main.qss");
+    qss.open(QFile::ReadOnly);
+    if( qss.isOpen() )
+    {
+        qApp->setStyleSheet( qss.readAll() );
+    }
 }
 
 MainWindow::~MainWindow()
--- tool/star_rpg_editor/star_rpg_editor/star_rpg_editor.pro
+++ tool/star_rpg_editor/star_rpg_editor/star_rpg_editor.pro
@@ -11,10 +11,13 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 TARGET = star_rpg_editor
 TEMPLATE = app
 
-
+CONFIG += console
 SOURCES += main.cpp\
         mainwindow.cpp
 
 HEADERS  += mainwindow.h
 
 FORMS    += mainwindow.ui
+
+RESOURCES += \
+    main.qrc
